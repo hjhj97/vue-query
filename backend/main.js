5ababaedccf8a9fecc59e9a8718dd1c906e4a3d5
JuHeonHa <hjhjaueon97@naver.com>
2023-01-09 16:57:46 +0900

    update

--- backend/main.js
+++ backend/main.js
@@ -1,68 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
 
 const cors = require("cors");
 app.use(cors());
+
+const list = [
+  {
+    name: "aa",
+    age: 10,
+  },
+  {
+    name: "bb",
+    age: 20,
+  },
+];
 
 app.get("/", (request, response) => {
   response.send("Hello");
 });
 
-app.get("/getTest", (request, response) => {
-  const json = {
-    success: true,
-    data: "hello",
-  };
-  response.send(json);
+app.get("/getList", (request, response) => {
+  response.send(list);
 });
 
-app.get("/getEventList", (request, response) => {
-  console.log("getEventeList" + request);
-  const json = {
-    success: true,
-    list: [
-      {
-        event_id: 1,
-        title: "ㅇㅇㅇ 가맹점 이벤트",
-        content: "최대 6000원 캐시백",
-      },
-      {
-        event_id: 2,
-        title: "소비it 친구초대 이벤트",
-        content: "친구도 나도 5천원 받자!",
-      },
-    ],
-  };
-  response.send(json);
-});
-app.get("/getEventDetail", (request, response) => {
-  const json = {
-    success: true,
-    list: {
-      event_id: 1,
-      title: "ㅇㅇㅇ 가맹점 이벤트",
-      content: "최대 6000원 캐시백",
-      event_start_dt: "2022-10-19",
-      event_end_dt: "2022-10-22",
-    },
-  };
-  response.send(json);
-});
-
-app.get("/getNoticeList", (request, response) => {
-  console.log("getNoticeList" + request);
-  const json = {
-    success: true,
-    list: [
-      {
-        notice_id: 1,
-        title: "소비 it 오픈 공지",
-        content: "안녕하세요 소비it 팀입니다.",
-        reg_dt: "2022-10-20",
-      },
-    ],
-  };
-  response.send(json);
+app.post("/addItem", (request, response) => {
+  list.push(request.body);
+  console.log(list);
+  response.send({
+    sucesss: true,
+  });
 });
 
 app.listen(3000, () => console.log("listening port 3000"));
